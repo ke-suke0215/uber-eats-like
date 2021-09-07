21fa2c81af5c13faf47ae9ca7f608b2dd625b854
ke-suke0215 <keisuke.yohs.0215.1209@gmail.com>
2021-09-08 08:49:51 +0900

    注文モーダル仮作成 6-4

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,4 +1,13 @@
-import React, { Fragment, useEffect, useReducer } from 'react';
+import React, { Fragment, useEffect, useReducer, useState } from 'react';
+
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+import { LocalMallIcon } from '../components/Icons';
+import { FoodWrapper } from '../components/FoodWrapper';
+import Skeleton from '@material-ui/lab/Skeleton';
+
+import { FoodOrderDialog } from '../components/FoodOrderDialog';
 
 // reducers
 import {
@@ -10,10 +19,52 @@ import {
 // api
 import { fetchFoods } from '../apis/foods';
 
+// images
+import MainLogo from '../images/logo.png';
+import FoodImage from '../images/food-image.jpg';
+
+// constants
 import { REQUEST_STATE } from '../constants';
+import { COLORS } from '../style_constants';
+
+const BagIconWrapper = styled.div`
+  padding: 24px;
+`;
+
+const ColorBagIcon = styled(LocalMallIcon)`
+  color: ${COLORS.MAIN};
+`;
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 8px 32px;
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`;
+
+const FoodList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  margin-bottom: 50px;
+`;
+
+const ItemWrapper = styled.div`
+  margin: 16px;
+`;
 
 export const Foods = ({ match }) => {
   const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
+
+  const initialState = {
+    isOpenOrderDialog: false,
+    selectedFood: null,
+    selectedFoodCount: 1,
+  };
+  const [state, setState] = useState(initialState);
 
   useEffect(() => {
     // ロード中にする
@@ -32,19 +83,59 @@ export const Foods = ({ match }) => {
 
   return (
     <Fragment>
+      <HeaderWrapper>
+        <Link to="/restaurants">
+          <MainLogoImage src={MainLogo} alt="main logo" />
+        </Link>
+        <BagIconWrapper>
+          <Link to="/orders">
+            <ColorBagIcon fontSize="large" />
+          </Link>
+        </BagIconWrapper>
+      </HeaderWrapper>
+      <FoodList>
+        {
+          foodsState.fetchState === REQUEST_STATE.LOADING ?
+            <Fragment>
+              {
+                [...Array(12).keys()].map(i =>
+                  <ItemWrapper key={i}>
+                    <Skeleton key={i} variant="rect" width={450} height={180} />
+                  </ItemWrapper>
+                )
+              }
+            </Fragment>
+            :
+            foodsState.foodsList.map(food =>
+              <ItemWrapper key={food.id}>
+                <FoodWrapper
+                  food={food}
+                  onClickFoodWrapper={(food) =>
+                    setState({
+                      ...state,
+                      isOpenOrderDialog: true,
+                      selectedFood: food,
+                    })
+                  }
+                  imageUrl={FoodImage}
+                />
+              </ItemWrapper>
+            )
+        }
+      </FoodList>
+
       {
-        foodsState.fetchState === REQUEST_STATE.LOADING ?
-          <Fragment>
-            <p>
-              ロード中...
-            </p>
-          </Fragment>
-          :
-          foodsState.foodsList.map(food =>
-            <div key={food.id}>
-              {food.name}
-            </div>
-          )
+        state.isOpenOrderDialog &&
+        <FoodOrderDialog
+          food={state.selectedFood}
+          isOpen={state.isOpenOrderDialog}
+          onClose={() =>
+            setState({
+              ...state,
+              isOpenOrderDialog: false
+            })
+          }
+        />
       }
     </Fragment>
   )
